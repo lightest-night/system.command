d628a4783e339d29c70807e0a0a0e5eefbbb7d2f
James Woodley <james@lightestnight.com>
2020-04-29 10:41:02 +0100

    Add AddCommandValidator to ExtendsServiceCollection

---
+++ Command/ExtendsServiceCollection.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using LightestNight.System.ServiceResolution;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LightestNight.System.Command
+{
+    public static class ExtendsServiceCollection
+    {
+        public static IServiceCollection AddCommandValidators(IServiceCollection services, params Assembly[] validatorAssemblies)
+        {
+            AssemblyScanning.RegisterServices(services, validatorAssemblies, new[]
+            {
+                new ConcreteRegistration
+                {
+                    InterfaceType = typeof(ICommandValidator<>),
+                    AddIfAlreadyExists = true
+                }
+            });
+
+            return services;
+        }
+    }
+}
